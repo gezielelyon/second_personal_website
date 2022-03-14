4b53e8b3a5dac92d557b0ed05b4925ea3dad29ac
Geziel Elyon <geziel_barbosa_@hotmail.com>
2022-03-14 05:37:35 -0300

    feat: add next-seo on project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
+import { useState } from 'react';
 import type { GetStaticProps, NextPage } from 'next';
 import NextImage from 'next/image';
 import NextLink from 'next/link';
 import dynamic from 'next/dynamic';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
+import { NextSeo } from 'next-seo';
 
 import { IHeaderProps } from '@components/Header/index';
-
-const Header = dynamic<IHeaderProps>(() =>
-  import('@components/Header/index').then(module => module.Header),
-);
+import { lightTheme, darkTheme } from '@styles/index';
 
 import {
   Container,
@@ -26,8 +25,10 @@ import {
   ThirdListButton,
   ThirdButtonWithIcon,
 } from '@styles/Home/index';
-import { useState } from 'react';
-import { lightTheme, darkTheme } from '@styles/index';
+
+const Header = dynamic<IHeaderProps>(() =>
+  import('@components/Header/index').then(module => module.Header),
+);
 
 interface IHomeProps {
   currentLanguage: string;
@@ -41,135 +42,145 @@ const Home: NextPage<IHomeProps> = ({ currentLanguage }) => {
   );
 
   return (
-    <Container
-      className={currentThemeMode === 'light' ? lightTheme : darkTheme}
-    >
-      <title>Geziel Elyon</title>
-
-      <Header
-        currentLanguage={currentLanguage}
-        currentThemeMode={currentThemeMode}
-        setCurrentThemeMode={setCurrentThemeMode}
+    <>
+      <NextSeo
+        title="Geziel Elyon software engineer website"
+        description="This is the Geziel Elyon software engineer website and contain some important link."
       />
+      <Container
+        className={currentThemeMode === 'light' ? lightTheme : darkTheme}
+      >
+        <title>Geziel Elyon</title>
 
-      <MainContainerContent>
-        <FirstSection>
-          <DeveloperNameTitle>
-            Geziel Elyon <br /> Barbosa Costa
-          </DeveloperNameTitle>
+        <Header
+          currentLanguage={currentLanguage}
+          currentThemeMode={currentThemeMode}
+          setCurrentThemeMode={setCurrentThemeMode}
+        />
 
-          <FistSectionImageFigure>
-            <NextImage
-              src="/geziel.jpg"
-              height={150}
-              width={150}
-              title="Geziel Elyon"
-              alt="Image Geziel Elyon"
-            />
-          </FistSectionImageFigure>
-        </FirstSection>
+        <MainContainerContent>
+          <FirstSection>
+            <DeveloperNameTitle>
+              Geziel Elyon <br /> Barbosa Costa
+            </DeveloperNameTitle>
 
-        <SecondSection>
-          <SecondSectionTitle>
-            {t('common:title_second_section')} 🔥
-          </SecondSectionTitle>
-          <DescriptionText>
-            {t('common:description_second_section')}
-          </DescriptionText>
-        </SecondSection>
+            <FistSectionImageFigure>
+              <NextImage
+                src="/geziel.jpg"
+                height={150}
+                width={150}
+                title="Geziel Elyon"
+                alt="Image Geziel Elyon"
+              />
+            </FistSectionImageFigure>
+          </FirstSection>
 
-        <ThirdSection>
-          <ThirdSectionTitle>
-            {t('common:title_third_section')} 👇
-          </ThirdSectionTitle>
+          <SecondSection>
+            <SecondSectionTitle>
+              {t('common:title_second_section')} 🔥
+            </SecondSectionTitle>
+            <DescriptionText>
+              {t('common:description_second_section')}
+            </DescriptionText>
+          </SecondSection>
 
-          <ThirdListButton>
-            <ThirdButtonWithIcon>
-              <NextLink
-                href="https://github.com/programador404"
-                passHref
-                prefetch={false}
-              >
-                <a title="Github link" aria-label="Github link" target="_blank">
-                  <h3>Github</h3>
-                  <span>🖥️</span>
-                </a>
-              </NextLink>
-            </ThirdButtonWithIcon>
-            <ThirdButtonWithIcon>
-              <NextLink
-                href="https://www.linkedin.com/in/geziel-elyon-a0a1381a5/"
-                passHref
-                prefetch={false}
-              >
-                <a
-                  title="Linkedin link"
-                  aria-label="Linkedin link"
-                  target="_blank"
+          <ThirdSection>
+            <ThirdSectionTitle>
+              {t('common:title_third_section')} 👇
+            </ThirdSectionTitle>
+
+            <ThirdListButton>
+              <ThirdButtonWithIcon>
+                <NextLink
+                  href="https://github.com/programador404"
+                  passHref
+                  prefetch={false}
                 >
-                  <h3>Linkedin</h3>
-                  <span>🤓</span>
-                </a>
-              </NextLink>
-            </ThirdButtonWithIcon>
-            <ThirdButtonWithIcon>
-              <NextLink
-                href="https://www.instagram.com/gezielelyon"
-                passHref
-                prefetch={false}
-              >
-                <a
-                  title="Instagram link"
-                  aria-label="Instagram link"
-                  target="_blank"
+                  <a
+                    title="Github link"
+                    aria-label="Github link"
+                    target="_blank"
+                  >
+                    <h3>Github</h3>
+                    <span>🖥️</span>
+                  </a>
+                </NextLink>
+              </ThirdButtonWithIcon>
+              <ThirdButtonWithIcon>
+                <NextLink
+                  href="https://www.linkedin.com/in/geziel-elyon-a0a1381a5/"
+                  passHref
+                  prefetch={false}
                 >
-                  <h3>Instagram</h3>
-                  <span>😎</span>
-                </a>
-              </NextLink>
-            </ThirdButtonWithIcon>
-            <ThirdButtonWithIcon>
-              <NextLink
-                href="https://maisretorno.com/"
-                passHref
-                prefetch={false}
-              >
-                <a
-                  title="Empresa atual link"
-                  aria-label="Empresa atual link"
-                  target="_blank"
+                  <a
+                    title="Linkedin link"
+                    aria-label="Linkedin link"
+                    target="_blank"
+                  >
+                    <h3>Linkedin</h3>
+                    <span>🤓</span>
+                  </a>
+                </NextLink>
+              </ThirdButtonWithIcon>
+              <ThirdButtonWithIcon>
+                <NextLink
+                  href="https://www.instagram.com/gezielelyon"
+                  passHref
+                  prefetch={false}
                 >
-                  <h3>{t('common:company_text')}</h3>
-                  <span>👨‍💻</span>
+                  <a
+                    title="Instagram link"
+                    aria-label="Instagram link"
+                    target="_blank"
+                  >
+                    <h3>Instagram</h3>
+                    <span>😎</span>
+                  </a>
+                </NextLink>
+              </ThirdButtonWithIcon>
+              <ThirdButtonWithIcon>
+                <NextLink
+                  href="https://maisretorno.com/"
+                  passHref
+                  prefetch={false}
+                >
+                  <a
+                    title="Empresa atual link"
+                    aria-label="Empresa atual link"
+                    target="_blank"
+                  >
+                    <h3>{t('common:company_text')}</h3>
+                    <span>👨‍💻</span>
+                  </a>
+                </NextLink>
+              </ThirdButtonWithIcon>
+              <ThirdButtonWithIcon>
+                <a
+                  href={
+                    currentLanguage === 'English'
+                      ? '/english-curriculum.pdf'
+                      : '/portuguese-curriculum.pdf'
+                  }
+                  title="Baixar currículo"
+                  aria-label="Baixar currículo"
+                  download
+                >
+                  <h3>{t('common:curriculum_text')}</h3>
+                  <span>💯</span>
                 </a>
-              </NextLink>
-            </ThirdButtonWithIcon>
-            <ThirdButtonWithIcon>
-              <a
-                href={
-                  currentLanguage === 'English'
-                    ? '/english-curriculum.pdf'
-                    : '/portuguese-curriculum.pdf'
-                }
-                title="Baixar currículo"
-                aria-label="Baixar currículo"
-                download
-              >
-                <h3>{t('common:curriculum_text')}</h3>
-                <span>💯</span>
-              </a>
-            </ThirdButtonWithIcon>
-          </ThirdListButton>
-        </ThirdSection>
-      </MainContainerContent>
+              </ThirdButtonWithIcon>
+            </ThirdListButton>
+          </ThirdSection>
+        </MainContainerContent>
 
-      <HomeFooter>
-        <span>
-          {t('common:text_footer')} <wbr />
-          <strong>Geziel Elyon</strong>
-        </span>
-      </HomeFooter>
-    </Container>
+        <HomeFooter>
+          <span>
+            {t('common:text_footer')} <wbr />
+            <strong>Geziel Elyon</strong>
+          </span>
+        </HomeFooter>
+      </Container>
+    </>
   );
 };
 
